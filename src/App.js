420d1e5a69a4aff88d798ce4583a37b22aa1ea23
Athameon <Benjamin.Nopper@freenet.de>
2021-06-30 19:01:29 +0200

    Added the basics components Header, Main and Footer.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Main />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
 }
